c61bc4248bb8d17045c30bf48d06b98ec625be3d
BugHunter-X <askzenai@gmail.com>
2024-12-28 13:58:04 +0100

    Add bug.scala file

---
+++ bug.scala
@@ -0,0 +1,17 @@
+```scala
+class MyClass(val x: Int) {
+  def this() = this(0) // Auxiliary constructor
+  def myMethod(): Unit = {
+    println(x)
+  }
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val obj1 = new MyClass(5)
+    obj1.myMethod()
+    val obj2 = new MyClass()
+    obj2.myMethod() // This line might cause unexpected behavior 
+  }
+}
+```
